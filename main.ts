48bb95cfcb70435f5bbebbebc2161cca66f0564c
Wiliyanto <wiliyanto@grip-principle.com>
2025-04-23 20:53:07 +0700

    feat: Add folder check and read file from request

--- main.ts
+++ main.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from 'express';
 import { getUser } from './test.controller';
+import { pdfUpload } from './pdf-upload';
 const serverPort = process.env.SERVER_PORT ?? 5000;
 const app = express();
 
 app.get('/', getUser);
-app.get('/convert-to-pdf', getUser);
+app.post('/convert-to-pdf', pdfUpload.single('file'), (req, res) => {
+  if (!req.file) {
+    res.send('Invalid file');
+    return;
+  }
+  res.send(req.file);
+});
 
 //TODO: 1. Add multer
 //TODO: 2. Read pdf file, fs and stuff
--- pdf-upload.ts
+++ pdf-upload.ts
@@ -1,8 +1,12 @@
 import { Request } from 'express';
+import fs from 'fs';
 import multer from 'multer';
 
 const storage = multer.diskStorage({
   destination: function (req: Request, file, cb) {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
     cb(null, 'uploads/');
   },
   filename: function (req: Request, file, cb) {
