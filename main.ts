7e42d746dc949e829ec376e348e4d05bcc2c75b3
Wiliyanto <wiliyanto@grip-principle.com>
2025-04-26 21:26:31 +0700

    feat: Implement PDF conversion functionality with Docker

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import { getUser } from './test.controller';
 import { pdfUpload } from './pdf-upload';
+import { exec } from 'child_process';
+import path from 'path';
 const serverPort = process.env.SERVER_PORT ?? 5000;
 const app = express();
 
@@ -10,12 +12,24 @@ app.post('/convert-to-pdf', pdfUpload.single('file'), (req, res) => {
     res.send('Invalid file');
     return;
   }
-  res.send(req.file);
-});
 
-//TODO: 1. Add multer
-//TODO: 2. Read pdf file, fs and stuff
-//TODO: 3. Function to convert to pdf (route to docker)
+  const uploadFileName = req.file.filename;
+  const fileNameWithoutExt = path.parse(req.file.filename).name;
+  // why ./workspace?, its a directory mount to docker
+  const command = `docker exec libreoffice libreoffice --headless --convert-to pdf "./workspace/${uploadFileName}" --outdir "./workspace/converted"`;
+
+  exec(command, (error, stdout, stdin) => {
+    if (error) {
+      res.send('Failed to convert file');
+      return;
+    }
+
+    res.download(
+      `./uploads/converted/${fileNameWithoutExt}.pdf`,
+      `${fileNameWithoutExt}.pdf`
+    );
+  });
+});
 
 app.listen(serverPort, () => {
   console.log(`Listening on porte ${serverPort}`);
