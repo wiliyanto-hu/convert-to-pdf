766ddd8109f52a673639737061e1672455477e31
Wiliyanto <wiliyanto@grip-principle.com>
2025-05-17 19:18:04 +0700

    feat: Add environment check when run listen for test

--- main.ts
+++ main.ts
@@ -1,23 +1,25 @@
-import express from 'express';
-import { pdfUpload } from './middleware/pdf-upload';
-import { convertToPdf } from './controller/converter.controller';
-import { documentValidation } from './middleware/file-validation';
+import express from "express";
+import { pdfUpload } from "./middleware/pdf-upload";
+import { convertToPdf } from "./controller/converter.controller";
+import { documentValidation } from "./middleware/file-validation";
 
 const serverPort = process.env.SERVER_PORT ?? 5000;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hi');
+app.get("/", (req, res) => {
+  res.send("Hi");
 });
 app.post(
-  '/convert-to-pdf',
-  pdfUpload.single('file'),
+  "/convert-to-pdf",
+  pdfUpload.single("file"),
   documentValidation,
   convertToPdf
 );
 
-// TODO: 2. Remove file outside of try catch block
+if (process.env.NODE_ENV !== "test") {
+  app.listen(serverPort, () => {
+    console.log(`Listening on porte ${serverPort}`);
+  });
+}
 
-app.listen(serverPort, () => {
-  console.log(`Listening on porte ${serverPort}`);
-});
+export default app;
